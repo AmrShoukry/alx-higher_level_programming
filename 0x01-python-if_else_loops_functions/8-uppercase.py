6a6bd6642030fab0a1d43ff80f639165ba280ed3
Amr Shoukry <shoukryworkamr1@gmail.com>
2023-05-09 14:15:45 -0700

    task8 fix

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -4,5 +4,5 @@ def uppercase(string):
         value = ord(c)
         if value >= 97 and value <= 122:
             value = value - 32
-        print("{:c}".format(value), end ="")
+        print("{:c}".format(value), end="")
     print("")
