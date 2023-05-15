2c7c2eecd51fcbd1a4f0a263d09544831bab111b
Amr Shoukry <shoukryworkamr1@gmail.com>
2023-05-15 06:13:02 -0700

    README

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,16 @@
+/**
+ * is_palindrome - checks if the linked list is palindrome or not
+ * @head: pointer to head of list
+ * Return: 0 for not being palindrome, 1 for palindrome
+ */
+
+
+
+int is_palindrome(listint_t **head)
+{
+	listint_t *current = head;
+	if (current == NULL)
+		return (1);
+
+
+}
