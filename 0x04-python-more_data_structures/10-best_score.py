d855ae53c1967ab13b7eb418aed9391a080c7bc3
Amr Shoukry <shoukryworkamr1@gmail.com>
2023-05-17 09:20:27 -0700

    task10

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    big = "None"
+    big_int = None
+    if a_dictionary != None:
+        for key, value in a_dictionary.items():
+            if big_int is None or value > big_int:
+                big_int = value
+                big = key
+    return big
