06e402405743ee7c979fd8b70ada9bb594de613f
Amr Shoukry <shoukryworkamr1@gmail.com>
2023-05-17 09:21:52 -0700

    task10 fix

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,7 +2,7 @@
 def best_score(a_dictionary):
     big = "None"
     big_int = None
-    if a_dictionary != None:
+    if a_dictionary is not None:
         for key, value in a_dictionary.items():
             if big_int is None or value > big_int:
                 big_int = value
