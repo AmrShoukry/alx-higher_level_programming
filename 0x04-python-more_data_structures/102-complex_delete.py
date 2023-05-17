16b2b4292a5543596789fb68b6c3eeefd588853b
Amr Shoukry <shoukryworkamr1@gmail.com>
2023-05-17 12:55:03 -0700

    task 102

---
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def complex_delete(a_dictionary, value):
+    if a_dictionary == None:
+        return a_dictionary
+    keys = []
+    for a_key, a_value in a_dictionary.items():
+        if a_value == value:
+            keys.append(a_key)
+    for key in keys:
+        a_dictionary.pop(key)
