d412e6c18e51f2b2bd6e17dc920a0a40184de9b2
Amr Shoukry <shoukryworkamr1@gmail.com>
2023-05-30 01:16:47 +0300

    task1 fix

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -3,5 +3,6 @@
 def safe_print_integer(value):
     try:
         print("{:d}".format(value))
+        return True
     except Exception:
-        print(f"{value} is not an integer")
+        return False
