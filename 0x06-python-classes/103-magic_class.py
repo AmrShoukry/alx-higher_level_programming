6ec725f61dbf8c54f585dfe956faa3f6cbdf31a8
Amr Shoukry <shoukryworkamr1@gmail.com>
2023-06-01 13:33:05 +0300

    task103

---
+++ 0x06-python-classes/103-magic_class.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" Task103 """
+
+
+import math
+    
+class MagicClass:
+    """ Class documentation """
+
+    def __init__(self, radius):
+        if type(radius) is not int or type(radius) is not float:
+            raise TypeError("radius must be a number")
+        self.__radius = radius
+
+    def area(self):
+        return self.__radius * 2 * math.pi
+
+    def circumference(self):
+        return 2 * math.pi * self.__radius
