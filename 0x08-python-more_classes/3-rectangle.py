c5996c85c485b2aeb98982a993439a4b6df04a7e
Amr Shoukry <shoukryworkamr1@gmail.com>
2023-06-06 02:06:40 +0300

    task3 fix

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -24,8 +24,6 @@ class Rectangle:
                 string += "#"
             if row != self.__height - 1:
                 string += "\n"
-        if string == "":
-            string += "\n"
         return string
 
     @property
