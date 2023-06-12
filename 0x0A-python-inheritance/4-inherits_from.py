5ef90df2c7b46d9c8c9004879004f95b1d56ca9f
Amr Shoukry <shoukryworkamr1@gmail.com>
2023-06-12 14:02:48 +0300

    task4 fix

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -2,7 +2,7 @@
 """ Module """
 
 
-def is_kind_of_class(obj, a_class):
+def inherits_from(obj, a_class):
     """ Method """
 
-    return isinstance(obj, a_class)
+    return isinstance(obj, a_class) and type(obj) != a_class
