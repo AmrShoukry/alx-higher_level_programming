634b52aec422f9a66b7552d019ffb81419edc659
Amr Shoukry <shoukryworkamr1@gmail.com>
2023-06-13 21:16:57 +0300

    task6 fix

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -6,5 +6,5 @@ import json
 
 def load_from_json_file(filename):
     """ Function """
-    with open(filename, "r") as file:
-        return json.load(filename)
+    with open(filename, "r", encoding="utf-8") as file:
+        return json.load(file)
