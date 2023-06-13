c0f8aed3b844bc46c0d568d6a8837877a78e3371
Amr Shoukry <shoukryworkamr1@gmail.com>
2023-06-13 22:45:20 +0300

    task100

---
+++ 0x0B-python-input_output/100-append_after.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" Model """
+
+import json
+
+
+def append_after(filename="", search_string="", new_string=""):
+    """ Function """
+    string = ""
+    with open(filename, "r", encoding="utf-8") as file:
+        for line in file.readlines():
+            string += line
+            if search_string in line:
+                string += new_string
+
+    with open(filename, "w", encoding="utf-8") as file:
+        file.write(string)
+
