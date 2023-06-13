2a43f7e7058c935ae703f67ec15d14770aa57752
Amr Shoukry <shoukryworkamr1@gmail.com>
2023-06-13 21:24:24 +0300

    task7

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" Model """
+
+import json
+import sys
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('load_from_json_file').load_from_json_file
+
+args = []
+
+for arg in sys.argv:
+    args.append(arg)
+
+save_to_json_file(args, "add_item.json")
