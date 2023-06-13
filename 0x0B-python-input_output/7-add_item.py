8f549f54131bd841b50764a607c5723132006bca
Amr Shoukry <shoukryworkamr1@gmail.com>
2023-06-13 21:29:11 +0300

    task7 fix

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -4,11 +4,11 @@
 if __name__ == '__main__':
     import sys
     save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-    load_from_json_file = __import__('load_from_json_file').load_from_json_file
+    load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
     args = []
 
-    for arg in sys.argv:
+    for arg in sys.argv[1:]:
         args.append(arg)
 
     save_to_json_file(args, "add_item.json")
