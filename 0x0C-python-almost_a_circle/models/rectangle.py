10b5c6368312d6cc35a836bc5dbaf4aa4cae3cab
Amr Shoukry <shoukryworkamr1@gmail.com>
2023-06-19 18:31:32 +0300

    fix bugs

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 """ Rectanlge Class Module """
 
 from models.base import Base
--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 """ Square Class Model """
 
 from models.rectangle import Rectangle
