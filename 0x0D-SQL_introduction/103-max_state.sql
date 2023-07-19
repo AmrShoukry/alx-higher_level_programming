d6654ccd146fa5276cf905fb14e37029a1ef3432
Amr Shoukry <shoukryworkamr1@gmail.com>
2023-07-19 10:30:56 +0300

    task 103

--- 0x0D-SQL_introduction/103-max_state.sql
+++ 0x0D-SQL_introduction/103-max_state.sql
@@ -1,2 +1,2 @@
 -- task103
-SELECT `state`, MAX(`temperature`) AS `max_temp` GROUP BY `state` ORDER BY `state`
+SELECT `state`, MAX(`value`) AS `max_temp` FROM `temperatures` GROUP BY `state` ORDER BY `state`
