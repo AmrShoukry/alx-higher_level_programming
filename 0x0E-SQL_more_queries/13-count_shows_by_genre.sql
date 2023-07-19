0c47d87feb939c00b3687149bcd62e2e36fc229d
Amr Shoukry <shoukryworkamr1@gmail.com>
2023-07-19 16:39:42 +0300

    task13 fix

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -2,4 +2,4 @@
 SELECT `tv_genres`.`name` AS `genre`, COUNT(*) AS `number_of_shows` 
 FROM `tv_genres` INNER JOIN `tv_shows_genres` ON `tv_genres`.`id` = `tv_shows_genres`.`genre_id`
 GROUP BY `genre`
-ORDER BY `genre` DESC
+ORDER BY `number_of_shows` DESC
