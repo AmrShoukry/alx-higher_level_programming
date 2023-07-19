ba87db7aaf3b1c50fd01f1e673df7447e4b68929
Amr Shoukry <shoukryworkamr1@gmail.com>
2023-07-19 16:23:03 +0300

    task9 fix

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,4 +1,4 @@
 -- Write a script that lists all cities contained in the database hbtn_0d_usa.
-SELECT `id`, `cities`.`name` AS `name`, `states`.`name` AS `name`
+SELECT `cities`.`id`, `cities`.`name` AS `name`, `states`.`name` AS `name`
 FROM `cities` INNER JOIN `states` ON `cities`.`state_id` = `states`.`id`
 ORDER BY `id`
