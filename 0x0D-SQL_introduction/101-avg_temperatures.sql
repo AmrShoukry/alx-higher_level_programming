1902d15bd4a403ed23b205f6482ea13cce7c66d8
Amr Shoukry <shoukryworkamr1@gmail.com>
2023-07-19 10:26:29 +0300

    Task 101

--- 0x0D-SQL_introduction/101-avg_temperatures.sql
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -1,2 +1,2 @@
 -- task101
-SELECT `city`, AVG(`temperature`) AS `avg_temp` GROUP BY `city` ORDER BY `avg_temp` DESC
+SELECT `city`, AVG(`value`) AS `avg_temp` FROM `temperatures` GROUP BY `city` ORDER BY `avg_temp` DESC
