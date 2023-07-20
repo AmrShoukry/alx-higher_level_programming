4e05bafa118b61a52afbc6267576d1f295f92667
AmrShoukry <shoukryworkamr1@gmail.com>
2023-07-20 12:11:00 +0300

    task14 fix

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -1,5 +1,6 @@
 -- Write a script that lists all genres from hbtn_0d_tvshows and displays the number of shows linked to each.
-SELECT `tv_genres.name` AS `genre`, COUNT(`tv_shows_genres.genre_id`) AS `number_of_shows` 
-FROM `tv_genres` INNER JOIN `tv_shows_genres` ON `tv_genres.id` = `tv_shows_genres.genre_id`
-GROUP BY `genre`
-ORDER BY `genre` DESC
+SELECT `tv_genres`.`name`
+FROM `tv_show_genres` 
+INNER JOIN `tv_genres` ON `tv_show_genres`.`genre_id` = `tv_genres`.`id`
+INNER JOIN `tv_shows` ON `tv_show_genres`.`show_id` = `tv_shows`.`id`
+WHERE `tv_shows`.`title` = 'Dexter'
