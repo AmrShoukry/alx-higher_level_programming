6718d2146c140b230f0bd956516e0d635c8a0d98
Amr Shoukry <shoukryworkamr1@gmail.com>
2023-08-14 19:28:39 +0300

    11. Second biggest!

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+const argv = require('process').argv;
+
+const numbers = argv.map(function (value) {
+  return parseInt(value);
+}).filter(function (value) {
+  return !isNaN(value);
+});
+
+if (numbers.length < 2) {
+  console.log(0);
+}
+
+let maxIndex = numbers.indexOf(Math.max(...numbers));
+numbers.splice(maxIndex, 1);
+maxIndex = numbers.indexOf(Math.max(...numbers));
+console.log(numbers[maxIndex]);
