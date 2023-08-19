966309749133af2410607d4f96d61543266f81e2
Amr Shoukry <shoukryworkamr1@gmail.com>
2023-08-19 18:36:07 +0300

    7. All states via SQLAlchemy fix

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-""" 6. First state model """
+""" 7. All states via SQLAlchemy """
 import sys
-from sqlalchemy import connection, Column, Integer, String
+from sqlalchemy import create_engine, Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
