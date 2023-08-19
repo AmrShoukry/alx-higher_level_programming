5a945b303b5dd7dc5b10801ac44867ad794f63dd
Amr Shoukry <shoukryworkamr1@gmail.com>
2023-08-19 16:49:53 +0300

    3. SQL Injection...

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+""" 3. SQL Injection... """
+import sys
+import MySQLdb
+
+
+if __name__ == '__main__':
+    DB_HOST = 'localhost'
+    DB_USER = sys.argv[1]
+    DB_PASS = sys.argv[2]
+    DB_NAME = sys.argv[3]
+    DB_PORT = 3306
+    STATE = sys.argv[4]
+
+    CONNECTION = MySQLdb.connect(
+        host=DB_HOST,
+        port=DB_PORT,
+        user=DB_USER,
+        passwd=DB_PASS,
+        db=DB_NAME
+    )
+
+    CURSOR = CONNECTION.cursor()
+
+    query = "SELECT * FROM states WHERE name LIKE BINARY %s ORDER BY id ASC"
+    arguments = (STATE, )
+                   
+    CURSOR.execute(query, arguments)
+
+    result = CURSOR.fetchall()
+    for row in result:
+        print(row)
+
+    CURSOR.close()
+    CONNECTION.close()
