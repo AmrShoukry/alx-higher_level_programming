f4e871ab6a5fc557f5607f7c5be66a316b9fbb71
Amr Shoukry <shoukryworkamr1@gmail.com>
2023-08-19 19:13:59 +0300

    11. Add a new state fix

--- 0x0F-python-object_relational_mapping/11-model_state_insert.py
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -6,12 +6,16 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 
-
+import os
+ROOT_PATH = os.path.dirname(os.path.realpath(__file__))
+DB_URI = os.path.join(ROOT_PATH, 'my_database.db')
+engine = create_engine(f"sqlite:///{DB_URI}", echo=True)
+    
 if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
-        sys.argv[1],
-        sys.argv[2],
-        sys.argv[3]), pool_pre_ping=True)
+    # engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+    #     sys.argv[1],
+    #     sys.argv[2],
+    #     sys.argv[3]), pool_pre_ping=True)
     Base.metadata.create_all(engine)
 
     session = sessionmaker(bind=engine)()
@@ -20,6 +24,6 @@ if __name__ == "__main__":
     session.add(new_state)
     session.commit()
 
-    print(new_State.id)
+    print(new_state.id)
 
     session.close()
