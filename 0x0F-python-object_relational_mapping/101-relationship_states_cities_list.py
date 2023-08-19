df84660df2a288625a3d319758481d0268025803
Amr Shoukry <shoukryworkamr1@gmail.com>
2023-08-19 21:13:54 +0300

    16. List relationship

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -6,21 +6,26 @@ from sqlalchemy.orm import sessionmaker
 from relationship_state import Base, State
 from relationship_city import City
 
+import os
+ROOT_PATH = os.path.dirname(os.path.realpath(__file__))
+DB_URI = os.path.join(ROOT_PATH, 'my_database.db')
+engine = create_engine(f"sqlite:///{DB_URI}")
 
 if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
-        sys.argv[1],
-        sys.argv[2],
-        sys.argv[3]), pool_pre_ping=True)
+    # engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+    #     sys.argv[1],
+    #     sys.argv[2],
+    #     sys.argv[3]), pool_pre_ping=True)
     Base.metadata.create_all(engine)
 
     session = sessionmaker(bind=engine)()
 
-    states = session.query(State).all()
+    states = session.query(State).order_by(State.id.asc()).all()
 
     for state in states:
         print(f"{state.id}: {state.name}")
-        for city in state.cities:
+        sorted_cities = sorted(state.cities, key=lambda city: city.id)
+        for city in sorted_cities:
             print(f"    {city.id}: {city.name}")
 
     session.close()
