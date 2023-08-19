91230c15fcc60750f2e07333ef8a62fa0737216f
Amr Shoukry <shoukryworkamr1@gmail.com>
2023-08-19 06:23:47 +0300

    1. Filter states

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+import sys
+import MySQLdb
+# import mysql.connector
+""" 1. Filter states """
+
+
+if __name__ == '__main__':
+    DB_HOST = 'localhost'
+    DB_USER = sys.argv[1]
+    DB_PASS = sys.argv[2]
+    DB_NAME = sys.argv[3]
+
+    connection = MySQLdb.connect(
+        host=DB_HOST,
+        user=DB_USER,
+        passwd=DB_PASS,
+        db=DB_NAME
+    )
+
+    # connection = mysql.connector.connect(
+    #     host=DB_HOST,
+    #     user=DB_USER,
+    #     password=DB_PASS,
+    #     database=DB_NAME
+    # )
+
+    cursor = connection.cursor()
+
+    cursor.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id asc")
+    result = cursor.fetchall()
+
+    for row in result:
+        print(row)
+
+    cursor.close()
+    connection.close()
