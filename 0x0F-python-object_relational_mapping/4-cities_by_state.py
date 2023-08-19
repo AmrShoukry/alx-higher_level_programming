f5ca01950241a113bbe24adc19ae6bdf8174fab4
Amr Shoukry <shoukryworkamr1@gmail.com>
2023-08-19 16:56:25 +0300

    4. Cities by states

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+""" 4. Cities by states """
+import sys
+import MySQLdb
+
+
+if __name__ == '__main__':
+    DB_HOST = 'localhost'
+    DB_USER = sys.argv[1]
+    DB_PASS = sys.argv[2]
+    DB_NAME = sys.argv[3]
+    DB_PORT = 3306
+
+    CONNECTION = MySQLdb.connect(
+        host=DB_HOST,
+        port=DB_PORT,
+        user=DB_USER,
+        passwd=DB_PASS,
+        db=DB_NAME
+    )
+
+    CURSOR = CONNECTION.cursor()
+
+    CURSOR.execute("SELECT * FROM cities ORDER BY id ASC")
+
+    result = CURSOR.fetchall()
+    for row in result:
+        print(row)
+
+    CURSOR.close()
+    CONNECTION.close()
