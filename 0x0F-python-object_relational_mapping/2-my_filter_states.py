68b0d3efe4cbf23d4463eb3a3e6d3f380062a173
Amr Shoukry <shoukryworkamr1@gmail.com>
2023-08-19 16:37:12 +0300

    2. Filter states by user input

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+""" 2. Filter states by user input """
+import sys
+import MySQLdb
+
+
+if __name__ == '__main__':
+    DB_HOST = 'localhost'
+    DB_USER = sys.argv[1]
+    DB_PASS = sys.argv[2]
+    DB_NAME = sys.argv[3]
+    DB_PORT = 3306
+    STATE = sys.argv[4]
+
+    CONNECTION = MySQLdb.connect(
+        host=DB_HOST,
+        port=DB_PORT,
+        user=DB_USER,
+        passwd=DB_PASS,
+        db=DB_NAME
+    )
+
+    CURSOR = CONNECTION.cursor()
+
+    query = "SELECT * FROM states WHERE name = %s"
+    arguments = (sys.argv[4],)
+
+    CURSOR.execute(query, arguments)
+
+    result = CURSOR.fetchone()
+    print(result)
+
+    CURSOR.close()
+    CONNECTION.close()
