324b27f4068519e65becdeea0e355594479a8d76
Amr Shoukry <shoukryworkamr1@gmail.com>
2023-08-31 12:55:45 +0300

    5. cURL POST parameters fix

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # 5. cURL POST parameters   
-curl -s -d "email: test@gmail.com" -d "subject: I will always be here for PLD" -X POST -i $1| grep -A20030803 "POST params:"
+curl -s -d "email: test@gmail.com" -d "subject: I will always be here for PLD" -X POST -i $1 | grep -A99 "POST params:"
