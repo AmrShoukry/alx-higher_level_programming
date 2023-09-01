5e3034901104b21424526b9ea3c577719e6b109b
Amr Shoukry <shoukryworkamr1@gmail.com>
2023-09-01 08:54:22 +0300

    5. Response header value #1

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" 5. Response header value #1 """
+import requests
+import sys
+
+if __name__ == '__main__':
+    variable = 'X-Request-Id'
+    url = sys.argv[1]
+
+    response = requests.get(url)
+    value = response.headers[variable]
+
+    print(value)
