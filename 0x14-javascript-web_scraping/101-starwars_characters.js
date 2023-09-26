88304b41f36b600cc88e43df9c3fc9a1d730690c
Amr Shoukry <shoukryworkamr1@gmail.com>
2023-09-26 13:14:22 +0300

    101

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
 
 const request = require('request');
-const URL = `https://swapi-api.alx-tools.com/api/films/${process.argv[2]}/`;
+const URL = `https://swapi-api.alx-tools.com/api/films/`;
 
 request(URL, function (error, response, body) {
   if (error);
-  const CHARACTERS = JSON.parse(body).characters;
+  const CHARACTERS = JSON.parse(body).results[process.argv[2] - 1].characters;
   CHARACTERS.forEach(characterURL => {
     request(characterURL, function (error2, response2, body2) {
       console.log(JSON.parse(body2).name);
